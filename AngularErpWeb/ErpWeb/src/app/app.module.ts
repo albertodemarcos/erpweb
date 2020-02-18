29d07b672a6939925a3651d03ec46e3991255f7f
alber <alber@192.168.1.41>
2020-02-18 20:46:25 +0100

    Navbar App
    Se crea el navbar de la app.

--- AngularErpWeb/ErpWeb/src/app/app.module.ts
+++ AngularErpWeb/ErpWeb/src/app/app.module.ts
@@ -1,14 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
+// Rutas
+import { APP_ROUTING } from './app.routes';
+// Servicios
+
+// Componentes
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './componentes/navbar/navbar.component';
+import { FooterComponent } from './componentes/footer/footer.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    APP_ROUTING
   ],
   providers: [],
   bootstrap: [AppComponent]
