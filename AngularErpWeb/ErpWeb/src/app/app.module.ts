229bcf3fc3086f53faac1473d374fc50bfbadc3f
alber <alber@192.168.1.35>
2020-05-25 18:55:52 +0200

    Capa Vista - Angular

--- AngularErpWeb/ErpWeb/src/app/app.module.ts
+++ AngularErpWeb/ErpWeb/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    FooterComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule,
