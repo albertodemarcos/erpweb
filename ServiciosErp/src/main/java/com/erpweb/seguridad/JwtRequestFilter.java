0afbdfb5ed7338ab5f5c1668e2c3c6b3c82be499
alber <alber@DESKTOP-HDK7Q2E>
2020-08-23 11:40:59 +0200

    Editando Seguridad parte 1

--- ServiciosErp/src/main/java/com/erpweb/seguridad/JwtRequestFilter.java
+++ ServiciosErp/src/main/java/com/erpweb/seguridad/JwtRequestFilter.java
@@ -47,14 +47,62 @@ public class JwtRequestFilter extends OncePerRequestFilter {
 
             if (jwtUtil.validateToken(jwt, userDetails)) {
 
-                UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
-                usernamePasswordAuthenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
-                SecurityContextHolder.getContext().setAuthentication(usernamePasswordAuthenticationToken);
+                UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+                authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                SecurityContextHolder.getContext().setAuthentication(authenticationToken);
             }
+        }	
+        filterChain.doFilter(request, response);
+		/*
+		//Primero extraemos el token
+		final String authorizationHeader = request.getHeader("Authorization");
+		
+		String username = null;
+        String jwt = null;
+		
+        if (authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) {
+            jwt = authorizationHeader.substring(7);
+            username = jwtUtil.extractUsername(jwt);
+        }
+		
+		//Comprobamos si viene el username, si no viene directmente no seguimos
+        if(username == null) {
+        	
+        	filterChain.doFilter(request, response);
+        	return;
         }
         
-        filterChain.doFilter(request, response);
-		
+        //Si existe, comprobamos la sesion
+        if(username != null) {
+        	
+        	//Recuperamos la sesion
+        	HttpSession sessionNow = request.getSession();
+        	SecurityContext securityContext = (SecurityContext) sessionNow.getAttribute("SPRING_SECURITY_CONTEXT");
+        	
+        	if(securityContext != null) {
+        		
+        		UserDetails userDetails = (UserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        			
+    			filterChain.doFilter(request, response);
+        		
+        	}else {
+        		
+        		UserDetails userDetails = this.usuarioDetailsService.loadUserByUsername(username);
+        		
+        		if (jwtUtil.validateToken(jwt, userDetails)) {
+
+                    UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+                    authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                    SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+                    
+                    //Guardamos en sesion el contexto
+                    HttpSession createdSesion = request.getSession(true);
+                    createdSesion.setAttribute("SPRING_SECURITY_CONTEXT", securityContext);
+                }
+        		
+        		filterChain.doFilter(request, response);
+        	}
+        }*/
 	}
 
 }
